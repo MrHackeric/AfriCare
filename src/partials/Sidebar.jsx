cda249d7d167d17c0fc39d72f653915089f9790c
dsyengo <deniswilson028@gmail.com>
2024-07-21 03:05:54 +0300

    Community updates

--- src/partials/Sidebar.jsx
+++ src/partials/Sidebar.jsx
@@ -107,7 +107,7 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
               <li className={`px-3 py-2 rounded-sm mb-0.5 last:mb-0 ${pathname.includes('Simplify Policies') && 'bg-slate-900'}`}>
                 <NavLink
                   end
-                  to="/chartArea"
+                  to="/chatArea"
                   className={`block text-slate-200 truncate transition duration-150 ${pathname.includes('Simplifies Policies') ? 'hover:text-slate-200' : 'hover:text-white'
                     }`}
                 >
